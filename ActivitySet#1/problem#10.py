f115d47cd3bca7a5e9acdd459f9e19d4506514ad
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-06-12 09:05:33 +0000

    dic

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,14 @@
 # Dictionaries
 
+
 filename = "dataset/mbox-short.txt"
+count=dict()
+name = input("Enter the file name:")
+handle = open(name,'r')
+for line in handle:
+    if not line.startswith('From '): 
+        continue
+    words=line.split()
+    words=words[1]
+    count[words]=count.get(words,0)+1
+print(words, count[words])
