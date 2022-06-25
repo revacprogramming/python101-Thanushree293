6d7200be6d6aa1fe5e098431e7736ac09b4f084c
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-06-25 04:57:19 +0000

    add

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,12 +1,8 @@
-
-
 def add(a, b):
-    return  # ...
-
-
+    return(a+b)
 def main():
-    a = input  # ...
-    b = input  # ...
-
+    a = int(input("enter the a value"))
+    b = int(input("enter the b value"))
     c = add(a, b)
-    print  # ...
+    print("addition of two numbers {0}+{1} = {2}".format(a,b,c))
+main()
