690c7002208964b5385d981e93b1316231a92f8b
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-06-26 15:15:14 +0000

    gfv

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,15 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+from turtle import *
+ 
+bgcolor('black')
+speed(0)
+hideturtle()
+for i in range(120):
+     color('red')
+     circle(i)
+     color("orange")
+     circle(i*0.9)
+     right(3)
+     forward(5)
+done()
