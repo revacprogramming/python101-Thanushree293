adbe9d425a124dc58679661dc7801647f009f1be
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-07-11 16:06:45 +0000

    both conversion of string 2 list

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,16 +1,40 @@
 
 
+#def get_cs():
+    #"""get string input"""
+
+
+#def cs_to_lot(cs):
+   # """convert connected string to list of strings"""
+
+
+#def lot_to_cs(lot):
+    #"""convert list of strings to connected string"""
+
+
+#def main():
+   # cs=get_cs()
+
+  #  lot=cs_to_lot(cs)  # convert connect string to list of tuples
+   # print(lot)
+
+   # cs=lot_to_cs(lot)  # convert list of strings to connect string
+   # print(cs)
+
+
+#if __name__ == '__main__':
+   # main()
 def get_cs():
-    """get string input"""
-
-
+    return(input())
+  
 def cs_to_lot(cs):
-    """convert connected string to list of strings"""
-
-
+    j=[]
+    ll=cs.split(';')
+    for i in ll:
+       j.append(tuple(i.split('=')))
+    return j
 def lot_to_cs(lot):
-    """convert list of strings to connected string"""
-
+    return (";".join([str(a+"="+b) for a,b in lot]))
 
 def main():
     cs=get_cs()
