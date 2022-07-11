b7fac07a7215c4565e874b73955c5b52a72a8a57
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-07-11 03:55:41 +0000

    m;l

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,16 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+from turtle import *
+ 
+bgcolor('black')
+speed(0)
+hideturtle()
+for i in range(120):
+     color('red')
+     circle(i)
+     color("orange")
+     circle(i*0.9)
+     right(3)
+     forward(5)
+done()
+3fedfv
