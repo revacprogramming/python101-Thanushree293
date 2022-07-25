1a96790142ccd0e7ce1cb8b7ca4c686e82561923
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-07-25 12:59:35 +0000

    file

--- ActivitySet#1/problem#22.py
+++ ActivitySet#1/problem#22.py
@@ -1,7 +1,17 @@
-from turtle import *
-colors = ['orange', 'red', 'pink', 'yellow', 'blue', 'green'] 
-for x in range(360):
-  pencolor(colors[x % 6])
-  width(x / 5 + 1)
-  forward(x)
-  left(20)
+#from turtle import *
+#colors = ['orange', 'red', 'pink', 'yellow', 'blue', 'green'] 
+#for x in range(360):
+  #pencolor(colors[x % 6])
+  #width(x / 5 + 1)
+  #forward(x)
+ # left(20)
+  '''02'''
+from urllib.request import urlopen
+import json
+url='http://py4e-data.dr-chuck.net/comments_1550211.json'
+jsFile = urlopen(url)
+data =json.loads(jsFile.read())
+sum=0
+for i in data['comments']:
+    sum+=i['count']
+print(sum)
